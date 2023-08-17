90f4cea8463825a70c95253775d92f0611a03a28
Gokul Pathak <gokul_pathak@yahoo.com>
2023-08-17 13:17:17 +0545

    preparing the edit view

--- resources/views/posts/Edit.blade.php
+++ resources/views/posts/Edit.blade.php
@@ -3,9 +3,10 @@
 
 @section('content')
     <h1>Edit Post</h1>
-    <form method="POST" action="/posts">
+    <form method="POST" action="/posts/{{ $post->id }}">
         @csrf
-        <input type="text" name="title" placeholder="enter title...">
+        <input type="hidden" name="_method" value="PUT">
+        <input type="text" name="title" placeholder="enter title..." value="{{ $post->title }}">
         <input type="submit" name="submit">
     </form>
 @endsection
