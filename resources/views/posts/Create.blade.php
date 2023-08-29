b47990da352dbb2ce6995d7195f12de05e6cb18a
Gokul Pathak <gokul_pathak@yahoo.com>
2023-08-29 17:03:02 +0545

    laravel collective added

--- resources/views/posts/Create.blade.php
+++ resources/views/posts/Create.blade.php
@@ -3,9 +3,10 @@
 
 @section('content')
     <h1>Create Post</h1>
-    <form method="POST" action="/posts">
-        @csrf
-        <input type="text" name="title" placeholder="enter title...">
-        <input type="submit" name="submit">
+    {{-- <form method="POST" action="/posts"> --}}
+    {!! Form::open() !!}
+    @csrf
+    <input type="text" name="title" placeholder="enter title...">
+    <input type="submit" name="submit">
     </form>
 @endsection
